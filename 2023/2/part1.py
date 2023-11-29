5702f18b29419aaf4e88688b105c563d3bc5ee2c
Camty1 <cameron.j.wolfe.23@dartmouth.edu>
2023-11-29 15:06:37 -0800

    added day 2.  oopsie a day late

--- 2023/2/part1.py
+++ 2023/2/part1.py
@@ -1,3 +1,34 @@
 # Day 2 of Advent of Code 2023 - Part 1
 #
 # Cameron Wolfe 12/2/2023
+
+TEST = 'test1.txt'
+FILE_NAME = 'day2.txt'
+
+num_blocks = {'red': 12, 'green': 13, 'blue': 14}
+sum = 0
+
+with open(FILE_NAME, 'r') as file:
+
+    while line := file.readline().strip():
+        
+        line_split = line.split(":")
+        game_number = int(line_split[0].split(" ")[1].strip())
+        game_pieces = line_split[1].strip()
+
+        valid = True
+        for grab in game_pieces.split(";"):
+            grab = grab.strip()
+            for subgrab in grab.split(","):
+                subgrab = subgrab.strip()
+                split_subgrab = subgrab.split(" ")
+                amount = int(split_subgrab[0])
+                color = split_subgrab[1]
+                if amount > num_blocks[color]:
+                    valid = False
+
+        if valid:
+            sum += game_number
+
+
+print(sum)
--- 2023/2/part2.py
+++ 2023/2/part2.py
@@ -1,3 +1,33 @@
 # Day 2 of Advent of Code 2023 - Part 2
 #
 # Cameron Wolfe 12/2/2023
+
+TEST = 'test1.txt'
+FILE_NAME = 'day2.txt'
+
+num_blocks = {'red': 12, 'green': 13, 'blue': 14}
+sum = 0
+
+with open(FILE_NAME, 'r') as file:
+
+    while line := file.readline().strip():
+        blocks_present = {'red': 0, 'green': 0, 'blue': 0}        
+        line_split = line.split(":")
+        game_number = int(line_split[0].split(" ")[1].strip())
+        game_pieces = line_split[1].strip()
+
+        for grab in game_pieces.split(";"):
+            grab = grab.strip()
+            for subgrab in grab.split(","):
+                subgrab = subgrab.strip()
+                split_subgrab = subgrab.split(" ")
+                amount = int(split_subgrab[0])
+                color = split_subgrab[1]
+                blocks_present[color] = max(blocks_present[color], amount)
+        set_power = 1
+        for _, amount in blocks_present.items():
+            set_power *= amount
+        sum += set_power
+
+
+print(sum)
